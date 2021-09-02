f0851c59d5a1a0a15da1b0ab6ba9b813b8683ff4
Radosław Brzozowski <radoslaw.brzozowski@gmail.com>
2021-09-02 14:38:36 +0200

    add models

---
+++ src/main/java/com/example/app/models/Hospital.java
@@ -0,0 +1,13 @@
+package com.example.app.models;
+
+public class Hospital {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/app/models/Patient.java
@@ -0,0 +1,13 @@
+package com.example.app.models;
+
+public class Patient {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
